4f5aa4c09fed5168d230d56e8516215739a3f32e
Thomas <thomasantapaola@gmail.com>
2023-02-07 15:29:50 +0100

    ConditionBean commit 1

---
+++ src/main/java/it/uniroma2/ispw/cardEmporium/Bean/ConditionBean.java
@@ -0,0 +1,13 @@
+package it.uniroma2.ispw.cardEmporium.Bean;
+
+public class ConditionBean {
+    private String condition_bean;
+
+    public String getCondition_bean() {
+        return condition_bean;
+    }
+
+    public void setCondition_bean(String condition_bean) {
+        this.condition_bean = condition_bean;
+    }
+}
