57356d17c5835ae90722bdc90a5a9104f92bc36b
Thomas <thomasantapaola@gmail.com>
2023-03-29 10:47:33 +0200

    aggiunto due classi §(exceptionswitchpage and switchpage

---
+++ src/main/java/it/uniroma2/ispw/cardemporium/exception/ExceptionSwitchpage.java
@@ -0,0 +1,9 @@
+package it.uniroma2.ispw.cardemporium.exception;
+
+public class ExceptionSwitchpage extends Exception {
+    public ExceptionSwitchpage(String s) {
+
+        super(s);
+
+    }
+}
---
+++ src/main/java/it/uniroma2/ispw/cardemporium/ui/SwitchPage.java
@@ -0,0 +1,42 @@
+package it.uniroma2.ispw.cardemporium.ui;
+
+
+
+
+import it.uniroma2.ispw.cardemporium.exception.ExceptionSwitchpage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SwitchPage {
+
+
+    private static final SwitchPage instance = new SwitchPage();
+
+    public static SwitchPage getInstance(){
+        return instance;
+    }
+    public void switchPage(String value, ActionEvent event) throws IOException, ExceptionSwitchpage {
+
+        try {
+            Parent viewRegister = FXMLLoader.load(getClass().getResource(value + ".fxml"));
+            Scene viewRegisterScene = new Scene(viewRegister);
+
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            window.setScene(viewRegisterScene);
+            window.show();
+
+        } catch (IOException e) {
+            throw new ExceptionSwitchpage("error switch page ");
+        }
+
+
+    }
+
+}
