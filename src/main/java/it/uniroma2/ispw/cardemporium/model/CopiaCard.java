0fc82b083c9104de947a5ca5c63189f70ce80037
Thomas <thomasantapaola@gmail.com>
2023-11-09 18:31:01 +0100

    risolto problema search carddao

--- src/main/java/it/uniroma2/ispw/cardemporium/model/CopiaCard.java
+++ src/main/java/it/uniroma2/ispw/cardemporium/model/CopiaCard.java
@@ -84,22 +84,7 @@ public class CopiaCard {
 
 
 
-    public CopiaCard(String condizione1, double prezzo, String utentevenditore, int cartasingolaID, int cartaID, String nomecarta, String s, String lingua, int versione, String setNome, boolean carrello,String extra, boolean venduto,int quantity){
-        this.condizione = condizione1;
-        this.prezzo = prezzo;
-        this.utenteVenditore = utentevenditore;
-        this.cartaSingolaID = cartasingolaID;
-        this.cartaID = cartaID;
-        this.nomeCarta = nomecarta;
-        this.nomeGioco = s;
-        this.lingua = lingua;
-        this.versione = versione;
-        this.nomeSet = setNome;
-        this.nelCarrello = carrello;
-        this.q=quantity;
-        this.extra = extra;
-        this.venduto = venduto;
-    }
+
     public CopiaCard(String condizione1, double prezzo, String utentevenditore,  int cartasingolaID, int cartaID, String nomecarta, String nomeGioco, String lingua, int versione, String setNome, boolean carrello,String extra, boolean venduto){
         this.condizione = condizione1;
         this.prezzo = prezzo;
