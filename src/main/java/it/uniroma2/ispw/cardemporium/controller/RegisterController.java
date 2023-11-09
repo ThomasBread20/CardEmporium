b2a95c812749c676570018c820cc9f8488f828cc
simoguidato <116288819+simoguidato@users.noreply.github.com>
2023-11-09 17:57:20 +0100

    pulizie

--- src/main/java/it/uniroma2/ispw/cardemporium/controller/RegisterController.java
+++ src/main/java/it/uniroma2/ispw/cardemporium/controller/RegisterController.java
@@ -3,11 +3,9 @@ package it.uniroma2.ispw.cardemporium.controller;
 
 
 import it.uniroma2.ispw.cardemporium.dao.DatabaseLoReFacade;
-import it.uniroma2.ispw.cardemporium.exception.ExceptionDBerror;
 import it.uniroma2.ispw.cardemporium.exception.ExceptionUserAlreadyExist;
 import it.uniroma2.ispw.cardemporium.exception.IllegalStateException;
 
-import java.sql.SQLException;
 import java.util.Random;
 
 public class RegisterController {
@@ -20,7 +18,7 @@ public class RegisterController {
 
 
 
-    public static void insertUserDao(String username, String passw, String name, java.sql.Date date, String surname) throws SQLException, ExceptionUserAlreadyExist, ExceptionDBerror {
+    public static void insertUserDao(String username, String passw, String name, java.sql.Date date, String surname) throws  ExceptionUserAlreadyExist {
 
 
 
